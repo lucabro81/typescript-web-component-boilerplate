e3f3dd3229dd7b34fd464c8292271670b3de77cb
Luca Brognara <luca.brognara@comperio.it>
2021-04-05 16:29:26 +0200

    fixed hello world example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,3 @@
 import { wrap } from './utils';
+import observedAttributesHelloworld from './components/hello-world/hello-world.observed-attributes';
+customElements.define('hello-world', wrap(()=>import('./components/hello-world/hello-world.component'), 'HelloWorld', observedAttributesHelloworld));
